bdbb6512257d51a75777a9c5a578d99107e65e48
Kehaar-II <kappleravec2p@gmail.com>
2023-10-01 14:34:14 +0200

    ft help fx combiner.py README.md | implemented sort by date and rewrote readme

--- README.md
+++ README.md
@@ -7,15 +7,18 @@ ezImageGrid requires the followings to work:
 - Pillow
 
 ## Usage
-- place all images in a folder, number in the desired order (images do not need to have to be zero-padded and can use all formats supported by PIL, images must be numbered continuously, the numbering can start at either 0 or 1)
-- run the following command
-```bash
-$ python3 combiner.py path [width]
+
+```
+$ python3 combiner.py path_to_image_folder [-w|--width nrows] [-s|--sort number|date] [-c|--center top|bottom|both]
+$ python3 combiner.py -h|--help
 ```
 
-### Description
-path: path to the folder containing the numbered images <br>
-width: nb of images per row
+### Options
+
+-w, --width _nrows_ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  number of images per row in the final grid <br>
+-s, --sort _number_|_date_ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sort the images in the grid either by the number in their filename or by the creation date of the file (for _date_, non zero-padded filename and zero-padding filename up to 4 digit are supported and can be mixed, filetypes can also be mixed)<br>
+~~-c, --center _top_|_bottom_|_both_ &nbsp;&nbsp;&nbsp;&nbsp;  if there is any incomplete row, centers it in the chosen location(s) (not centered by default)~~ <br>
+-h, --help &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;prints this message
 
 
 written in Python 3.11.3
--- combiner.py
+++ combiner.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import sys
 import os
+import glob
 from math import ceil
 
 supportedFormats = [".png", ".jpg", ".jpeg", ".gif", ".ppm", ".tiff", ".bmp", ".PNG", ".JPG", ".JPEG", ".GIF", ".PPM", ".TIFF", ".BMP"]
@@ -17,15 +18,19 @@ def cropImage(img):
     if ratio > desiredRation:
         img = img.resize((desiredSize[0], int(height / (width / desiredSize[0]))))
         cropAmount = (img.size[1] - desiredSize[1]) / 2
+        if (width <= 0):
+            return None
         img = img.crop((0, cropAmount, width, height - cropAmount))
     # image too wide, must use height as reference
     else:
         img = img.resize((int(width / (height / desiredSize[1])), desiredSize[1]))
         cropAmount = (img.size[0] - desiredSize[0]) / 2
+        if (width - cropAmount <= cropAmount):
+            return None
         img = img.crop((cropAmount, 0 , width - cropAmount, height))
     return img
 
-def assembleImages(imgs, width):
+def assembleImages(imgs, width, center):
     '''PIL image list, int -> PIL image'''
 
     print("combining images")
@@ -43,15 +48,28 @@ def assembleImages(imgs, width):
             currentHeight += desiredSize[1]
     return img
 
-def findImages(path):
-    '''string -> int'''
+def findImagesByDate(path):
+    '''string -> PIL image list'''
+    files = list(filter(os.path.isfile, glob.glob(path + "*")))
+    files.sort(key = lambda x: os.path.getmtime(x))
+    out = []
+    tmp = None
 
+    for file in files:
+        if (os.path.splitext(file)[1] in supportedFormats):
+            print(file.rsplit('/', 1)[-1])
+            tmp = cropImage(Image.open(file))
+            if tmp != None:
+                out.append(tmp)
+    return out
+
+def findImagesByName(path):
+    '''string -> PIL image list'''
     currentfile = None
     padding = ""
     hasFoundImageWithCurrentNb = True
     list = []
 
-    print("images found:")
     i = 0
     while hasFoundImageWithCurrentNb or i <= 1:
         hasFoundImageWithCurrentNb = False
@@ -74,43 +92,78 @@ def findImages(path):
         return None
     return list
 
+def findImages(path, byDate):
+    '''string, bool -> string[]'''
+    print("images found:")
+    if byDate:
+        return findImagesByDate(path)
+    else:
+        return findImagesByName(path)
+
+
+def printHelp():
+    print(open("./help", "r").read().replace("%progName%", sys.argv[0]))
+    return
+
 def verifyArgs():
-    '''void -> string, int
-    returns None, [error code] on errors
+    '''void -> string, int, String, String (width, sortMethode, center)
+    returns None, [error code], 0, 0 on errors
     -1 = no arguments
     -2 = not a directory
     -3 = width not a number
     -4 = too many arguments'''
 
+    width = 10
+    sortMethode = "number"
+    center = "none"
+
     av = sys.argv
     ac = len(av)
 
-    if ac == 1:
-        return None, -1
+    if ac == 1: # no path
+        printHelp()
+        return None, -1, 0, 0
+
+    if av[1] == "-h" or av[1] == "--help":
+        printHelp()
+        return None, 0, 0, 0
+
     path = av[1] if av[1][-1] == '/' else av[1] + '/'
-    if ac >= 2 and not os.path.isdir(path):
-        return None, -2
-    if ac == 2:
-        return path, 0
-    if ac == 3:
-        if not av[2].isnumeric():
-            return None, -3
-        elif int(av[2]) <= 0:
-            return None, -3
-        return path, int(av[2]) if int(av[2]) >= 0 else 0
-    return None, -4
+    if ac >= 2 and not os.path.isdir(path): # invalid path
+        printHelp()
+        return None, -2, 0, 0
+    if ac == 2: # only path
+        return path, width, sortMethode, center
+    if (ac % 2) != 0: # one of the options is wrong, don't need to check any further, may need to remove if adding support for giving files directly from the command
+        printHelp()
+        return None, -5, 0, 0
+
+    for i in range(2, ac, 2):
+        if av[i] in {"-w", "--width"}:
+            if not av[i + 1].isnumeric():
+                return None -3, 0, 0
+            width = int(av[i + 1]) if int(av[i + 1]) >= 0 else width
+        if av[i] in {"-s", "--sort"}:
+            if not av[i + 1] in {"number", "date"}:
+                return None, -6, 0, 0
+            sortMethode = av[i + 1]
+        if av[i] in {"-c", "--center"}:
+            if not av[i + 1] in {"top", "bottom", "both"}:
+                return None, -7, 0, 0
+            center = av[i + 1]
+    return path, width, sortMethode, center
 
 def main():
     '''void -> int'''
 
-    dir, width = verifyArgs()
+    dir, width, sortMethode, center = verifyArgs()
 
     if dir == None:
         return 0 - width
-    images = findImages(dir)
+    images = findImages(dir, True if sortMethode == "date" else False)
     if images == None:
         return 5
-    img = assembleImages(images, width if width > 0 else 10).save(dir + "output.png")
+    assembleImages(images, width, center).save(dir + "output.png")
     print("new image saved at " + dir + "output.png")
     return 0
 
